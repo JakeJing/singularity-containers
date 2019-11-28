2d86a5745b82f28ee5ca63d584a1d5cf133b8488
JakeJing <350956009@qq.com>
2019-11-28 11:36:35 +0100

    add slurm-sbatch.sh file

---
+++ slurm-sbatch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --qos=medium
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8GB
+module load generic
+# you can also use hpc, which is faster (see: https://www.zi.uzh.ch/en/teaching-and-research/science-it/infrastructure/cluster/cpu-partitions.html)
+module load singularity
+srun singularity exec -u r_latest.sif Rscript test.R
+
