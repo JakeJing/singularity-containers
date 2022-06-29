d7818e92dc095978ae36126bb4e8509d8278eeb4
JakeJing <350956009@qq.com>
2022-06-29 10:40:49 +0200

    update sh for array job

--- R/run-rscript.sh
+++ R/run-rscript.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #SBATCH --qos=medium
 #SBATCH --time=01:00:00
-#SBATCH --array=[1-12]
 #SBATCH --nodes=1
 #SBATCH --output=Rout/par-%J.out
 #SBATCH --error=Rout/par-%J.err
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2GB
+#SBATCH --array=[1-12]
+
 module load generic
 module load singularity
 
 # you can also use HPC or Hydra partitions, which are faster (see: https://www.zi.uzh.ch/en/teaching-and-research/science-it/infrastructure/sciencecluster/cpu-partitions.html)
 
 # for details use bash command >> module avail
+
+export id=$SLURM_ARRAY_TASK_ID
+echo $id
 
 srun singularity exec -u sandbox/singularity-r Rscript R_intro.R Rout/example_slurm${SLURM_ARRAY_TASK_ID}.Rout
